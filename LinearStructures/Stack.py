4f24f664eccf0082b6ab57c9299d0b9f067e48d1
StognienkoVladislav <stognienkovv@gmail.com>
2017-07-15 16:08:13 +0400

    LinearStructures

---
+++ LinearStructures/Stack.py
@@ -0,0 +1,34 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+if __name__ == '__main__':
+    s = Stack()
+
+    print(s.isEmpty())
+    s.push(4)
+    s.push('cat')
+    print(s.peek())
+    s.push(False)
+    print(s.size())
+    print(s.isEmpty())
+    s.push(8.5)
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
