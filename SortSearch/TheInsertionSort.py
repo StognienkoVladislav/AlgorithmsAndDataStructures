64a165c0c9eaf16d44c4768c2ed00563a761c445
StognienkoVladislav <stognienkovv@gmail.com>
2017-07-20 22:00:10 +0400

    Shell

---
+++ SortSearch/TheInsertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(alist):
+    for index in range(1, len(alist)):
+
+        currentvalue = alist[index]
+        position = index
+
+        while position > 0 and alist[position - 1] > currentvalue:
+            alist[position] = alist[position - 1]
+            position = position - 1
+
+        alist[position] = currentvalue
+
+if __name__ == '__main__':
+    alist = [42, 26, 92, 17, 77, 31, 44, 52, 20]
+    insertionSort(alist)
+    print(alist)
